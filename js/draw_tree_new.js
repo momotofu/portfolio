8fa17285098f0dc3d2b9dcf4c6a70a3c226b3495
Christopher Reece <mrkita.reece@gmail.com>
2017-12-08 16:11:09 +0900

    added breakpoints in draw_tree.py

--- js/draw_tree_new.js
+++ js/draw_tree_new.js
@@ -7,20 +7,21 @@ wood_color = "#795548"
 
 def branch(t, length, angle, scale):
     if length < 4:
+      if randint(1,4) > 1:
         t.color(leaf_colors[randint(0,4)])
         t.begin_fill()
         t.circle(randint(1,3))
         t.end_fill()
         t.color(wood_color)
-        return
+      return
     random_degree = randint(-5, 5)
     t.pensize(int(2.1 * length * scale * .07))
     t.forward(length)
     t.left(angle + random_degree)
-    branch(t, (randint(-15, 20) + length) * scale, angle + randint(-5, 5) * scale, scale - 0.05)
+    branch(t, (randint(-19, 40) + length) * scale, angle + randint(-5, 5) * scale, scale - 0.05)
     t.right(angle * 2 + random_degree)
-    branch(t, (randint(-15 - (length // 4), 20) + length) * scale, angle + randint(-5, 5) * scale, scale - 0.04)
-    branch(t, randint(-25, 10) * scale, randint(0, 45), scale)
+    branch(t, (randint(-14 - (length // 4), 20) + length) * scale, angle + randint(-5, 5) * scale, scale - 0.04)
+    branch(t, randint(-29, 10) * scale, randint(0, 45), scale)
     t.left(angle + 180)
     t.forward(length)
     t.left(180)
@@ -29,11 +30,11 @@ def draw_tree():
     t = turtle.Turtle()
     t.hideturtle()
     t.penup()
-    t.setpos(0, -200)
+    t.setpos(${window.visualViewport.width > 470 ? 0 : -210}, -300)
     t.pendown()
     t.color(wood_color)
     t.speed(0)
     t.left(90)
-    branch(t, 100, 20, 0.8)
+    branch(t, 100, 20, ${window.visualViewport.width > 470 ? 0.8 : 0.71})
 
 draw_tree()`})();
