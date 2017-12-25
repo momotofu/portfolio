a63ba6de78ff8e457219b386bc8cf182d2231176
Christopher Reece <mrkita.reece@gmail.com>
2017-12-25 16:02:55 +0900

    set up autoprefixer for css

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,13 @@
 var gulp = require('gulp')
 var stylus = require('gulp-stylus')
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('default', function() {
   gulp.src('./stylus/*')
     .pipe(stylus())
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
     .pipe(gulp.dest('./css'))
 });
