4b05e8b8c9ef877604133c6f7b213bd4fffc53a1
Christopher Reece <mrkita.reece@gmail.com>
2017-12-25 15:47:37 +0900

    added gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
 var gulp = require('gulp')
+var stylus = require('gulp-stylus')
 
 gulp.task('default', function() {
   console.log("it's working.")
 })
+
+gulp.task('stylus', function() {
+  gulp.src('./stylus/*')
+    .pipe(stylus())
+    .pipe(gulp.dest('./css'))
+});
