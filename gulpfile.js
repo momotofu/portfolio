3b32abaae223f2c0c89a1629b213bb9d46565161
Christopher Reece <mrkita.reece@gmail.com>
2017-12-25 16:26:26 +0900

    added a watcher for css code

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,14 @@ var stylus = require('gulp-stylus')
 var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('default', function() {
+  var watcher = gulp.watch('./stylus/*', ['styles'])
+
+  watcher.on('change', function(event) {
+    console.log(`File ${event.path} was ${event.type}, running tasks...`)
+  })
+})
+
+gulp.task('styles', function() {
   gulp.src('./stylus/*')
     .pipe(stylus())
     .pipe(autoprefixer({
