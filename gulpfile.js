d2bd2de013f09ddc1be1e8b287cbc80478f2434c
Christopher Reece <mrkita.reece@gmail.com>
2017-12-25 15:57:56 +0900

    updated gulp.js file

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,6 @@ var gulp = require('gulp')
 var stylus = require('gulp-stylus')
 
 gulp.task('default', function() {
-  console.log("it's working.")
-})
-
-gulp.task('stylus', function() {
   gulp.src('./stylus/*')
     .pipe(stylus())
     .pipe(gulp.dest('./css'))
